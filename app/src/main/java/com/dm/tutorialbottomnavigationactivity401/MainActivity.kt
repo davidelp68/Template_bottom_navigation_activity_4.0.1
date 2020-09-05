9cd440c4062d170f65d57e40a78a0d44472b814a
davidelp68 <davide.malu@live.it>
2020-09-05 17:28:00 +0200

    Initial commit

---
+++ app/src/main/java/com/dm/tutorialbottomnavigationactivity401/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.dm.tutorialbottomnavigationactivity401
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passaggio di ogni ID menu come un insieme di ID perché ogni
+        // menu deve essere considerato come destinazioni di primo livello.
+        val appBarConfiguration = AppBarConfiguration(setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
